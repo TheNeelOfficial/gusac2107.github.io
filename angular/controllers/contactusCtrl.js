fef201d15d606eddc6f842f39c03c6e22ef31c7d
Ankit Bharadiya <asbharadiya@gmail.com>
2017-05-24 14:45:49 +0530

    address carousel

--- angular/controllers/contactusCtrl.js
+++ angular/controllers/contactusCtrl.js
@@ -1,6 +1,16 @@
 angular.module('paradox')
 	.controller('contactusCtrl', ['$rootScope', '$scope', '$http', '$state', function ($rootScope, $scope, $http, $state) {
 
-		
+		$scope.slickConfig = {
+			enabled: true,
+			infinite:false,
+			dots:true,
+			arrows:false,
+			slidesToShow:1,
+			slidesToScroll:1,
+			draggable: false,
+			autoplay:false,
+			speed: 1000
+		}
 		
 	}])
